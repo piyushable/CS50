b848252e3898cea7519ad740a8f14d2df912aec7
Piyush Jha <piyushjha@Piyushs-MacBook-Air.local>
2025-06-17 16:13:26 +0530

    Added week2

---
+++ week2/readability/readability.c
@@ -0,0 +1,81 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
+
+int main(void)
+{
+    string text = get_string("Text: ");
+
+    int letters = count_letters(text);
+    int words = count_words(text);
+    int sentences = count_sentences(text);
+
+    // L is the average number of letters per 100 words in the text.
+    // S is the average number of sentences per 100 words in the text.
+
+
+
+    float L = (float) letters / words * 100;
+    float S = (float) sentences / words * 100;
+
+    float index = 0.0588 * L - 0.296 * S - 15.8;
+    int grade = round(index);
+
+    if (grade < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (grade >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", grade);
+    }
+}
+
+int count_letters(string text)
+{
+    int count = 0;
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (isalpha(text[i]))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int count_words(string text)
+{
+    int count = 1; // Starts at 1 because last word doesn't follow a space
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (text[i] == ' ')
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int count_sentences(string text)
+{
+    int count = 0;
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            count++;
+        }
+    }
+    return count;
+}
